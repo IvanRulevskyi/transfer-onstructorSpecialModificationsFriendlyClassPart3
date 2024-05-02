2033cad38851cb1d264fbc287b43d6a1c27c1b50
IvanRulevskyi <rulevskiy4@gmail.com>
2024-05-02 14:32:49 +0300

    FinishTask

--- transferСonstructorSpecialModificationsFriendlyClassPart3/main.cpp
+++ transferСonstructorSpecialModificationsFriendlyClassPart3/main.cpp
@@ -1,14 +1,54 @@
-//
-//  main.cpp
-//  transferСonstructorSpecialModificationsFriendlyClassPart3
-//
-//  Created by Иван Рулевский on 02.05.2024.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//Створіть клас для представлення документа та клас для його керування (створення, редагування, збереження). Використовуйте friend class, щоб дозволити класу керування документами отримати доступ до внутрішніх структур документа.
+
+class AddValue;
+
+class Data
+{
+private:
+    vector<int> data;
+    
+public:
+    friend class AddValue;
+    
+    
+};
+
+class AddValue
+{
+public:
+    void addData(Data& value, int val)
+    {
+        value.data.emplace_back(val);
+    }
+    
+    void setData(Data& value, int index, int val)
+    {
+        value.data[index] = val;
+    }
+ 
+    void print(Data& value)
+    {
+        for (int i = 0 ; i < value.data.size(); i++)
+        {
+            cout<<value.data[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+
+int main()
+{
+    Data data;
+    AddValue add;
+    add.addData(data, 2);
+    add.addData(data, 3);
+    add.print(data);
+    add.setData(data, 0, 1);
+    add.print(data);
+    
 }
