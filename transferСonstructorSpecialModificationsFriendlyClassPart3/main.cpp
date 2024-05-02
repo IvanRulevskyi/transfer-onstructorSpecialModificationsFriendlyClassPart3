25053b81ad226c27d89ef89bd3e50519191be755
IvanRulevskyi <rulevskiy4@gmail.com>
2024-05-02 14:03:12 +0300

    Initial Commit

---
+++ transferСonstructorSpecialModificationsFriendlyClassPart3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  transferСonstructorSpecialModificationsFriendlyClassPart3
+//
+//  Created by Иван Рулевский on 02.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
